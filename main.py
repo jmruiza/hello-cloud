cb136c2a009080a4803e957eda0fbb6351155b98
JM Ruiz <jmanuel.rar@emeraldigital.com>
2020-11-09 17:44:52 -0600

    Change message

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    name = os.environ.get('NAME', 'World')
-    return 'Hello {}!'.format(name)
+    name = os.environ.get('NAME', 'Google Cloud')
+    return 'Hello {} - Powered by Emerald Digital!'.format(name)
 
 
 if __name__ == "__main__":
